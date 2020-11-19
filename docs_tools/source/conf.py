6fd3c6a2eb6cfbcae565c88e1429e4fc5b9637eb
Terry Chan <napoler2008@gmail.com>
2020-11-19 16:10:42 +0800

    mm

--- docs_tools/source/conf.py
+++ docs_tools/source/conf.py
@@ -70,8 +70,11 @@ extensions = [
 #
 #html_theme = 'alabaster'
 
-html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+#html_theme = "sphinx_rtd_theme"
+#html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+html_theme = "sphinx_book_theme"
+
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
